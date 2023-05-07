faf42c61cc974aa9646ba1cbd924a440fdd2638a
mjaworsky <markian.jaworsky@gmail.com>
2023-05-07 10:57:02 +1000

    Add files via upload

--- Baseline_v_FS_T_TESTS.R
+++ Baseline_v_FS_T_TESTS.R
@@ -1,64 +1,36 @@
 setwd('C:/Users/mdjaw/OneDrive/Documents/Markian Training/Research_USQ')
 
 
-> x <- c(0.15,0.01,0.13,0.18,0.01)
-> y <- c(0.10,0.01,0.13,0.15,0.20) 
-> t.test(x, y)
+
+> 
+> baseline_2class_recall <- c(0.01,0.2,0.01,0.6,0.13,0,0,0,0.6,0,0.54,0.01,0,0,0,0.4,0,0,0,0,0,0.5,0.8,0.5,0.33,0.5,0.5,0.5,0,0.4,0,0,0,0,0)
+> 
+> fs_2class_recall <- c(0.51,0.5,0.52,0.5,0.62,0.5,0.5,0.5,0.5,1,0.5,1,0.55,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.55,0.5,0.52,0.5,0.63,0.52,0.5,0.5,0.5,0.5,0.53,0.67,0.5,0.5)
+> 
+> t.test(baseline_2class_recall, fs_2class_recall)
 
         Welch Two Sample t-test
 
-data:  x and y
-t = -0.45973, df = 7.8632, p-value = 0.6582
+data:  baseline_2class_recall and fs_2class_recall
+t = -7.6058, df = 48.679, p-value = 8.006e-10
 alternative hypothesis: true difference in means is not equal to 0
 95 percent confidence interval:
- -0.13268635  0.08868635
+ -0.4547729 -0.2646557
 sample estimates:
 mean of x mean of y 
-    0.096     0.118 
+0.1865714 0.5462857 
 
-> x <- c(0.70,0.49,0.44,0.50,0.50)
-> y <- c(0.93,0.92,0.44,0.87,0.51) 
-> t.test(x, y)
+> fs_2class_precision <- c(0.83,0.38,0.9,0.5,0.93,0.44,0.44,0.5,0.4,0.92,0.43,0.92,0.55,0.5,0.44,0.43,0.44,0.44,0.44,0.44,0.44,0.84,0.34,0.85,0.5,0.87,0.89,0.43,0.72,0.37,0.95,0.5,0.51,0.5,0.5)
+> t.test(baseline_2class_precision, fs_2class_precision)
 
         Welch Two Sample t-test
 
-data:  x and y
-t = -1.7954, df = 5.3712, p-value = 0.1285
+data:  baseline_2class_precision and fs_2class_precision
+t = -10.238, df = 66.687, p-value = 2.587e-15
 alternative hypothesis: true difference in means is not equal to 0
 95 percent confidence interval:
- -0.49972592  0.08372592
+ -0.5643713 -0.3802002
 sample estimates:
 mean of x mean of y 
-    0.526     0.734 
-
-> x <- c(0.50,0.50,0.50,0.50,0.50)
-> y <- c(0.62,1.00,0.50,0.63,0.67) 
-> t.test(x, y)
-
-        Welch Two Sample t-test
-
-data:  x and y
-t = -2.192, df = 4, p-value = 0.09348
-alternative hypothesis: true difference in means is not equal to 0
-95 percent confidence interval:
- -0.41705607  0.04905607
-sample estimates:
-mean of x mean of y 
-    0.500     0.684 
-
-> x <- c(0.47,0.50,0.47,0.49,0.50)
-> y <- c(0.67,0.96,0.47,0.67,0.58) 
-> t.test(x, y)
-
-        Welch Two Sample t-test
-
-data:  x and y
-t = -2.2553, df = 4.0557, p-value = 0.08621
-alternative hypothesis: true difference in means is not equal to 0
-95 percent confidence interval:
- -0.40929338  0.04129338
-sample estimates:
-mean of x mean of y 
-    0.486     0.670 
-
+0.1128571 0.5851429 
 
